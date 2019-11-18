65ac02eea7fee8e5f88eeddd1d87d5a0b682c260
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2019-11-18 17:23:49 +0100

    Changed folder structure and added some jsonwebtoken stuff

---
+++ backend/src/AuthHandler.ts
@@ -0,0 +1,30 @@
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+
+export class AuthHandler {
+    tokens: Map<String, Date>;
+    users: string[]
+
+    constructor() {
+        this.tokens = new Map<string, Date>();
+        this.users = [];
+    }
+    
+    public validToken(token: string): boolean {
+        if(this.tokens.has(token)) {
+            return this.tokens.get(token).valueOf() < Date.now() ? true : false;
+        } else {
+            return false;
+        }
+    }
+
+    public getToken(username: string, password: string): string {
+        if(this.users.includes(crypto.createHash("sha256").update(username + password).digest("hex"))) {
+            let secret = "my super secret";
+            let myHash = crypto.createHash("sha256").update(username + password).digest("hex");
+            crypto.create
+            let token = jwt.sign(myHash, secret, { algorithm: "HS512", encoding: "UTF-8", expiresIn: 60*5 });
+
+        }
+    }
+}
