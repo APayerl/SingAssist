21c11d5a53661bd87175cdc394c6720385d56218
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2019-12-27 17:19:18 +0100

    fixed db for users and credentials

--- backend/src/models/Credential.ts
+++ backend/src/models/Credential.ts
@@ -1,4 +1,21 @@
 import { BaseModel } from "./BaseModel";
+import { DataTypes, Sequelize } from "sequelize";
+
+export function CredentialInit(sequelize: Sequelize) {
+	Credential.init({
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, {
+		sequelize: sequelize,
+		tableName: 'credentials'
+	});
+}
 
 export class Credential extends BaseModel {
 	public email!: string;
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,6 +1,31 @@
-import { HasManyGetAssociationsMixin, HasManyHasAssociationMixin, HasManyCountAssociationsMixin, HasManyAddAssociationMixin, HasManyCreateAssociationMixin, Association } from 'sequelize';
+import { HasManyGetAssociationsMixin, 
+    HasManyHasAssociationMixin, 
+    HasManyCountAssociationsMixin, 
+    HasManyAddAssociationMixin, 
+    HasManyCreateAssociationMixin, 
+    Association, 
+    DataTypes, 
+    Sequelize } from 'sequelize';
 import { BaseModel } from './BaseModel';
 import { Credential } from './Credential';
+
+export function UserInit(sequelize: Sequelize) {
+    User.init({
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastname: {
+            type: new DataTypes.STRING(128),
+            allowNull: false
+        }
+    }, {
+        tableName: 'users',
+        sequelize: sequelize
+    });
+
+    // User.hasMany(Credential);
+}
 
 export class User extends BaseModel {
     public firstname!: string;
