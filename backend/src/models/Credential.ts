636e384845b780030bb57ba7a3b9f20ab75bf5ce
APayerl <6882326+APayerl@users.noreply.github.com>
2020-01-24 20:22:49 +0100

    Revert "enable use of tokens in database"
    This reverts commit c3d28908bebcda13a7de1dab44dd7d752f3378e4.

--- backend/src/models/Credential.ts
+++ backend/src/models/Credential.ts
@@ -2,7 +2,6 @@ import { BaseModel } from "./BaseModel";
 import { DataTypes, Sequelize } from "sequelize";
 
 export function CredentialInit(sequelize: Sequelize) {
-	let credArr = Object.keys(CredentialType);
 	Credential.init({
 		email: {
 			type: DataTypes.STRING,
@@ -11,15 +10,6 @@ export function CredentialInit(sequelize: Sequelize) {
 		password: {
 			type: DataTypes.STRING,
 			allowNull: true
-		},
-		token: {
-			type: DataTypes.STRING,
-			allowNull: true
-		},
-		type: {
-			type: DataTypes.ENUM,
-			values: credArr.slice(credArr.length/2),
-			allowNull: false
 		}
 	}, {
 		sequelize: sequelize,
@@ -27,14 +17,7 @@ export function CredentialInit(sequelize: Sequelize) {
 	});
 }
 
-export enum CredentialType {
-	EMAIL,
-	TOKEN
-}
-
 export class Credential extends BaseModel {
-	public type!: CredentialType;
-	public token?: string;
-	public email?: string;
-	public password?: string;
+	public email!: string;
+	public password!: string;
 }
